296c3db141bb6bb5b11adc81b49388933c237195
Dhruv <51574716+Illusion47586@users.noreply.github.com>
2023-10-05 13:11:54 +0530

    feat: type exports

--- src/stories/tab.stories.ts
+++ src/stories/tab.stories.ts
@@ -1,11 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Tab } from '..';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Tab } from "../components/tab";
 
 const meta = {
-  title: 'Components/Tab',
+  title: "Components/Tab",
   component: Tab,
-  parameters: { layout: 'centered' },
-  tags: ['autodocs'],
+  parameters: { layout: "centered" },
+  tags: ["autodocs"],
 } satisfies Meta<typeof Tab>;
 
 export default meta;
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +12,19 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "comp-lib",
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
+      plugins: [
+        dts({
+          exclude: ["node_modules/**", "src/stories/**"],
+          clearPureImport: true,
+        }),
+      ],
       treeshake: true,
       external: ["react", "react-dom"],
       output: {
+        exports: "named",
         globals: {
           react: "react",
           "react-dom": "reactDom",
